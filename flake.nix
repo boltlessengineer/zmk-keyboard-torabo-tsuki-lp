18dd56914f1c865a83a2cd6ef8b489db355abede
Seongmin Lee <git@boltless.me>
2026-06-22 14:29:54 +0900

    wip
    Signed-off-by: Seongmin Lee <git@boltless.me>

--- flake.nix
+++ flake.nix
@@ -6,9 +6,14 @@
       url = "github:lilyinstarlight/zmk-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    paw3222 = {
+      url = "path:///Users/boltless/repo/zmk-driver-paw3222";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, zmk-nix }: let
+  outputs = { self, nixpkgs, zmk-nix, paw3222 }: let
     forAllSystems = nixpkgs.lib.genAttrs (nixpkgs.lib.attrNames zmk-nix.packages);
   in {
     packages = forAllSystems (system: rec {
@@ -17,12 +22,25 @@
       firmware = zmk-nix.legacyPackages.${system}.buildSplitKeyboard {
         name = "firmware";
 
-        src = nixpkgs.lib.sourceFilesBySuffices self [ ".board" ".cmake" ".conf" ".defconfig" ".dts" ".dtsi" ".json" ".keymap" ".overlay" ".shield" ".yml" "_defconfig" ];
+        src = nixpkgs.lib.sourceFilesBySuffices self [ ".board" ".cmake" ".conf" ".defconfig" ".dts" ".dtsi" ".json" ".keymap" ".overlay" ".shield" ".yml" "_defconfig"
+          # to use source as zmk module
+          ".c" ".h" ".txt" ];
 
-        board = "nice_nano_v2";
-        shield = "lily58_%PART%";
+        board = "bmp_boost";
+        shield = "torabo_tsuki_lp_%PART%";
+        # enableZmkStudio = true;
+        snippets = ["studio-rpc-usb-uart"]; # explicitly set studio-rpc-usb-uart instead of enableZmkStudio so it is configured for both left and right side of the board.
+        parts = [
+          "left"
+          "right"
+        ];
+        centralPart = "right"; # not used since we aren't using enableZmkStudio
+        extraWestBuildFlags = [ ];
+        extraCmakeFlags = [
+          "-DZMK_EXTRA_MODULES=${paw3222}"
+        ];
 
-        zephyrDepsHash = "sha256-BOjA9u2HIbtXJHX9j92494HlE66LMmOya+ZWUq9OWYw=";
+        zephyrDepsHash = "sha256-0qMlYPRXLVlp5m0dao2A+4prbiAVLuNFElMderMXrII=";
 
         meta = {
           description = "ZMK firmware";
