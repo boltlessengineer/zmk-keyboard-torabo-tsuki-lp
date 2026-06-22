5aafba0a6f949a838d9f81d45452d1354f543e1c
Seongmin Lee <git@boltless.me>
2026-06-22 13:46:40 +0900

    wip: use zmk-nix
    Signed-off-by: Seongmin Lee <git@boltless.me>

--- flake.nix
+++ flake.nix
@@ -1,52 +1,42 @@
 {
-  description = "my zmk config";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-    zephyr.url = "github:zmkfirmware/zephyr/v3.5.0+zmk-fixes";
-    zephyr.flake = false;
-
-    zephyr-nix.url = "github:nix-community/zephyr-nix";
-    zephyr-nix.inputs.zephyr.follows = "zephyr";
-    zephyr-nix.inputs.nixpkgs.follows = "nixpkgs";
+    zmk-nix = {
+      url = "github:lilyinstarlight/zmk-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs @ { flake-parts, zephyr-nix, ... }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["aarch64-darwin"];
+  outputs = { self, nixpkgs, zmk-nix }: let
+    forAllSystems = nixpkgs.lib.genAttrs (nixpkgs.lib.attrNames zmk-nix.packages);
+  in {
+    packages = forAllSystems (system: rec {
+      default = firmware;
 
-      perSystem = {
-        pkgs,
-        system,
-        ...
-      }: let
-        zephyr = zephyr-nix.packages.${system};
-      in {
-        devShells.default = pkgs.mkShell {
-          packages = [
-            zephyr.pythonEnv
-            (zephyr.sdk-0_16.override { targets = ["arm-zephyr-eabi"]; })
+      firmware = zmk-nix.legacyPackages.${system}.buildSplitKeyboard {
+        name = "firmware";
 
-            # zephyr.hosttools
-            pkgs.cmake
-            pkgs.dtc
-            pkgs.ninja
-            pkgs.qemu
+        src = nixpkgs.lib.sourceFilesBySuffices self [ ".board" ".cmake" ".conf" ".defconfig" ".dts" ".dtsi" ".json" ".keymap" ".overlay" ".shield" ".yml" "_defconfig" ];
 
-            pkgs.just
-            pkgs.yq
-            pkgs.remarshal
-            pkgs.ccache
+        board = "nice_nano_v2";
+        shield = "lily58_%PART%";
 
-            pkgs.keymap-drawer
-          ];
-          env = {
-            # ZEPHYR_TOOLCHAIN_VARIANT = "gnuarmemb";
-            # GNUARMEMB_TOOLCHAIN_PATH = pkgs.gcc-arm-embedded;
-          };
+        zephyrDepsHash = "sha256-BOjA9u2HIbtXJHX9j92494HlE66LMmOya+ZWUq9OWYw=";
+
+        meta = {
+          description = "ZMK firmware";
+          license = nixpkgs.lib.licenses.mit;
+          platforms = nixpkgs.lib.platforms.all;
         };
       };
-    };
+
+      flash = zmk-nix.packages.${system}.flash.override { inherit firmware; };
+      update = zmk-nix.packages.${system}.update;
+    });
+
+    devShells = forAllSystems (system: {
+      default = zmk-nix.devShells.${system}.default;
+    });
+  };
 }
